1e1fe4274df55cf1a83c9f62d5253268cc01f5fa
ashpursglove <a.b.pursglove@gmail.com>
2021-07-02 20:58:58 +0100

    SMTP email set up

--- code/auto_email.py
+++ code/auto_email.py
@@ -1,25 +1,54 @@
-import smtplib, ssl
+import email, smtplib, ssl
 
-port = 587  # For starttls
-smtp_server = "smtp.gmail.com"
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+subject = "Such photos!!"
+body = "this is all from code!! that attached pic was encoded using 64bit encoding!"
+
+
+
 sender_email = "ashSMTPserver@gmail.com"
 #receiver_email = "a.b.pursglove@gmail.com"
 receiver_email = "catycastro1987@gmail.com"
 password = "AshandfidelSMTP"
-message = """\
-:-) !!!
 
-Woop woop!!
+# Create a multipart message and set headers
+message = MIMEMultipart()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+#message["Bcc"] = receiver_email  # Recommended for mass emails
 
-xxxxxxxxxxxxxxxxxxxxxxx
+# Add body to email
+message.attach(MIMEText(body, "plain"))
 
-"""
+filename = "image.jpg"  # In same directory as script
 
+# Open PDF file in binary mode
+with open(filename, "rb") as attachment:
+    # Add file as application/octet-stream
+    # Email client can usually download this automatically as attachment
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload(attachment.read())
+
+# Encode file in ASCII characters to send by email    
+encoders.encode_base64(part)
+
+# Add header as key/value pair to attachment part
+part.add_header(
+    "Content-Disposition",
+    f"attachment; filename= {filename}",
+)
+
+# Add attachment to message and convert message to string
+message.attach(part)
+text = message.as_string()
+
+# Log in to server using secure context and send email
 context = ssl.create_default_context()
-with smtplib.SMTP(smtp_server, port) as server:
-    server.ehlo()  # Can be omitted
-    server.starttls(context=context)
-    server.ehlo()  # Can be omitted
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
     server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
-    print("Sent")
+    server.sendmail(sender_email, receiver_email, text)
